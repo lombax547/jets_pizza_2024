f8c0a86b51a1e79e275f2ad3c82c00935e53d0e2
lombax547 <jaidenricksplace@gmail.com>
2024-11-20 09:30:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+#1. define your function
+def pizza_price():
 
+  #2. Ask customer what they want
+  requested_toppings = imput("""""")
+
+base_price = 15
+toppings_price = 0
+
+# tomatoes (+$1.50),
+# onions (+$1.25),
+# pineapple (+$3.50), 
+# mushrooms (+$3.75), and
+# avocado (+$0.40).
